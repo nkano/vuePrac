80ca27520bde66cf39c550214457395deb3e248a
kano <kano@team-lab.com>
2018-04-12 20:28:09 +0900

    hello world

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <button v-on:click="init()">reset</button>
   </div>
 </template>
 
 <script>
+const hello = 'Hello, World.'
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'nope'
+      msg: '',
+      index: 0,
+      wait: 0
+    }
+  },
+  created: function() {
+    setInterval(this.addLetter, 100)
+  },
+  methods: {
+    addLetter: function() {
+      if( this.wait <= 10 ) {
+        if( this.index < hello.length ) {
+          this.msg += hello[this.index]
+          this.index += 1
+        } else {
+          this.wait += 1
+        }
+      } else {
+        this.init()
+      }
+    },
+    init: function() {
+      this.index = 0
+      this.wait = 0
+      this.msg = ''
     }
   }
 }
@@ -19,14 +45,7 @@ export default {
 <style scoped>
 h1, h2 {
   font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
+  height: 30px;
 }
 a {
   color: #42b983;
